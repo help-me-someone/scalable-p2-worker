61e4250dc1494e685f576c2a1bbeabb3b9deb147
Appy <106456906+TheGreatWaves@users.noreply.github.com>
2023-10-25 20:24:47 +0700

    loading stuff via env

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"log"
 	"os"
 
+	db "github.com/help-me-someone/scalable-p2-db"
+	"github.com/help-me-someone/scalable-p2-db/models/user"
+	"github.com/help-me-someone/scalable-p2-db/models/video"
 	"github.com/help-me-someone/scalable-p2-worker/worker"
 	"github.com/hibiken/asynq"
 	"gorm.io/driver/mysql"
@@ -16,7 +20,25 @@ const (
 	REGION = "sgp1"
 )
 
+var (
+	DB_USERNAME string
+	DB_PASSWORD string
+	DB_IP       string
+	REDIS_IP    string
+	MODE        string
+)
+
+func loadEnvs() {
+	DB_USERNAME = os.Getenv("DB_USERNAME")
+	DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	DB_IP = os.Getenv("DB_IP")
+	REDIS_IP = os.Getenv("REDIS_IP")
+	MODE = os.Getenv("MODE")
+}
+
 func main() {
+	// Load from environment variables.
+	loadEnvs()
 
 	// Create temp folder for files to live in.
 	paths := []string{"temp", "temp/convert", "temp/thumbnail", "temp/save"}
@@ -29,13 +51,24 @@ func main() {
 		}
 	}
 
-	clientOpt := asynq.RedisClientOpt{Addr: "redis:6379"}
+	redisArr := fmt.Sprintf("%s:6379", REDIS_IP)
+	clientOpt := asynq.RedisClientOpt{Addr: redisArr}
 
-	// TODO: Load this via environments.
-	dsn := "user:password@tcp(mysql:3306)/toktik-db?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s)/toktik-db?charset=utf8mb4&parseTime=True&loc=Local",
+		DB_USERNAME,
+		DB_PASSWORD,
+		DB_IP,
+	)
 	toktik_db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Panicln("Error: Failed to connect to the database.")
+	}
+
+	// Initalize the database, incase it's not ready.
+	if !toktik_db.Migrator().HasTable(&user.User{}) && !toktik_db.Migrator().HasTable(&video.Video{}) {
+		db.InitTables(toktik_db)
+		log.Println("Database initialized!")
 	}
 
 	srv := asynq.NewServer(
